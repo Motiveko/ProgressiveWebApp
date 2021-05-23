4ef146eb8ad35d8ffd63eb5ff3588eebcaa3b4e6
MotiveKo <rhehdrla@naver.com>
2021-05-23 15:53:47 +0900

    사진 갤러리 앱 구현 ngrok를 이용한 로컬 테스트 서버의 외부 포워딩

--- README.md
+++ README.md
@@ -1,2 +1,72 @@
 # ProgressiveWebApp
 PWA 학습 레포지토리
+
+ - TODO_LIST App
+> https://motiveko-pwa.firebaseapp.com/
+
+ - 사진 갤러리 App
+
+
+## 학습 내용 정리
+
+---
+
+<br> 
+
+### Ch 10
+
+1. 서비스 워커의 런타임 캐시
+  - 워크박스의 5가지 캐시 전략
+    - Network-First
+    - Cache-First
+    - Stale-While-Revalidate
+    - Network-Only
+    - Cache-Only
+
+
+#### 1-1 Network-First
+> 인터넷 접속 성공 시 결과값을 보여주고, 실패 시 캐시를 보여준다. 항상 최신의 내용이 필요한 인터넷 기사 등에 적합
+
+#### 1-2 Cache-First
+>  캐시를 먼저 읽어서 보여준다. 캐시가 없다면 네트워크에 요청하고 결과값을 캐싱한다. 폰트, 이미지 등 거의 안바뀌는 내용에 적합.
+
+#### 1-3 StaleWhile-Revalidate
+> 캐시를 먼저 읽어서 보여준다. 동시에 네트워크에 요청해 결과값을 캐시와 비교 후 다르면 캐시를 업데이트한다. 업데이트가 가끔 발생하는 내용에 적합하다.
+
+#### 1-4 Network-Only
+> 무조건 네트워크 요청의 결과값을 보여준다. 내용이 자주 변해 캐시가 별로 의미 없는 경우 적합.
+
+#### 1-5 Cache-Only
+> 무조건 캐시에서 가져온 결과값을 보여준다. 오프라인 작업에 적합.
+
+
+2. ngrok를 이용한 local 서버 포워딩
+> ngrok를 이용하면 현재 로컬에서 개발중인 앱을 외부 모바일 기기 등을 통해 테스트 할 수 있다.
+
+#### 2-1 ngrok 설치
+
+```
+npm i -g ngrok
+```
+
+#### 2-2 vue.config.js의 설정
+
+```
+module.exports = {
+  ...
+
+  devServer: {
+    disableHostCheck: true
+  }
+}
+```
+
+#### 2-3 dev서버를 이용해 로컬  띄운 후 ngrok를 이용한 외부 포워딩
+```
+  npm run serve
+  ngrok http {port}
+```
+
+
+
+ 
---
+++ photo-app/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-app>
+    <v-app-bar color="blue-grey" dark fixed>
+      <v-app-bar-nav-icon ></v-app-bar-nav-icon>
+      <v-toolbar-title>사진 갤러리</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>more_vert</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-content>
+      <v-container>
+        <v-row class="mt-15">
+          <v-col cols="12" sm="6" md="4" lg="3" xl="2"
+                v-for="item in this.aPictures" :key=item.key>
+
+            <v-card height="330px">
+              <v-img :src="item.url" height="200px"></v-img>
+
+              <v-card-title>
+                <h1 class="title grey--text text--darken-3 mb-3"> {{item.title}}</h1>
+                <p class="body-2 grey--text"> {{item.info}}</p>
+              </v-card-title>
+
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+
+    <v-footer fixed>
+      <div class="mx-auto">&copy; motiveko</div>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+// @는 ~/src를의미
+import oPictureData from '@/datasources/picture-data'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      'aPictures' : oPictureData.aPictures
+    }
+  }
+};
+</script>
